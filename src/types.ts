bd44cf1832577f4fc8f993b28cf6b00846763759
jdecroock <decroockjovi@gmail.com>
2020-04-03 01:29:58 +0200

    add more types

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,18 @@
+export type OG =
+  | 'og:title'
+  | 'og:type'
+  | 'og:url'
+  | 'og:image'
+  | 'og:site_name'
+  | 'og:description'
+  | 'og:email'
+  | 'og:phone_number'
+  | 'og:fax_number'
+  | 'fb:admins'
+  | 'fb:app_id';
+
 export type Name =
+  | OG
   | 'application-name'
   | 'author'
   | 'description'
@@ -18,21 +32,36 @@ export type Name =
   | 'summary'
   | 'Classification'
   | 'designer'
-  | 'reply-to';
-export type Property =
-  | 'og:title'
-  | 'og:type'
-  | 'og:url'
-  | 'og:image'
-  | 'og:site_name'
-  | 'og:description'
-  | 'fb:admins'
-  | 'fb:app_id';
+  | 'reply-to'
+  | 'owner'
+  | 'url'
+  | 'identifier-URL'
+  | 'directory'
+  | 'category'
+  | 'coverage'
+  | 'distribution'
+  | 'rating'
+  | 'revisit-after';
+
+export type Property = OG;
 
 export type HttpEquiv =
   | 'content-type'
   | 'default-style'
   | 'refresh'
-  | 'content-security-policy';
+  | 'content-security-policy'
+  | 'Expires'
+  | 'Pragma'
+  | 'Cache-Control';
 
 export type CharSet = 'utf-8';
+
+export type Rel =
+  | 'shortcut icon'
+  | 'apple-touch-icon'
+  | 'apple-touch-startup-image'
+  | 'alternate'
+  | 'shortcut icon'
+  | 'fluid-icon'
+  | 'me'
+  | 'canonical';
