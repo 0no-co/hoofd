9abe05792ab45068cc1c47e2da2ece63a0b58140
jdecroock <decroockjovi@gmail.com>
2020-11-28 08:53:34 +0100

    add amp to the dom for prerender services

--- src/hooks/useAmp.ts
+++ src/hooks/useAmp.ts
@@ -1,8 +1,22 @@
 import dispatcher from '../dispatcher';
 import { isServerSide } from '../utils';
+import { useEffect } from 'react';
 
 export const useAmp = () => {
   if (isServerSide) {
     dispatcher._setAmp();
   }
+
+  useEffect(() => {
+    const nodeList = document.querySelectorAll(
+      'script[src="https://cdn.ampproject.org/v0.js"]'
+    );
+    document.getElementsByTagName('html')[0].setAttribute('amp', '');
+    if (!nodeList[0]) {
+      const ampScript = document.createElement('script');
+      ampScript.src = 'https://cdn.ampproject.org/v0.js';
+      ampScript.async = true;
+      document.head.insertBefore(ampScript, document.head.firstChild);
+    }
+  }, []);
 };
--- src/hooks/useHead.ts
+++ src/hooks/useHead.ts
@@ -102,6 +102,19 @@ export const useHead = ({ title, metas, language, amp }: HeadObject) => {
   }, [memoizedMetas]);
 
   useEffect(() => {
+    if (amp) {
+      const nodeList = document.querySelectorAll(
+        'script[src="https://cdn.ampproject.org/v0.js"]'
+      );
+      document.getElementsByTagName('html')[0].setAttribute('amp', '');
+      if (!nodeList[0]) {
+        const ampScript = document.createElement('script');
+        ampScript.src = 'https://cdn.ampproject.org/v0.js';
+        ampScript.async = true;
+        document.head.insertBefore(ampScript, document.head.firstChild);
+      }
+    }
+
     memoizedMetas.forEach((meta) => {
       dispatcher._addToQueue(META, meta);
     });
