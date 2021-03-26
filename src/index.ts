e87d20d8fef4246370213aa0a744448643f08ab6
Jovi De Croock <decroockjovi@gmail.com>
2021-03-26 19:23:03 +0100

    remove export

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,6 @@ export { createDispatcher } from './dispatcher';
 export * from './hooks/useLang';
 export * from './hooks/useLink';
 export * from './hooks/useMeta';
-export * from './hooks/useAmp';
 export * from './hooks/useTitle';
 export * from './hooks/useTitleTemplate';
 export { useHead } from './hooks/useHead';
